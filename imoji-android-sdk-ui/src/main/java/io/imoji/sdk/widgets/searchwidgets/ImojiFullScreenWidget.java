72f9caf3ae872d6e1200b41abfef7d86044683a2
Engin Dogusoy <engin@fleksy.com>
2016-04-25 12:34:17 +0300

    fixed full screen widget padding

--- imoji-android-sdk-ui/src/main/java/io/imoji/sdk/widgets/searchwidgets/ImojiFullScreenWidget.java
+++ imoji-android-sdk-ui/src/main/java/io/imoji/sdk/widgets/searchwidgets/ImojiFullScreenWidget.java
@@ -15,18 +15,24 @@ import io.imoji.sdk.widgets.searchwidgets.components.ImojiBaseSearchWidget;
  */
 public class ImojiFullScreenWidget extends ImojiBaseSearchWidget {
 
+    private final static int SPAN_COUNT = 4;
+
     public ImojiFullScreenWidget(Context context) {
-        super(context, 4, VERTICAL, true);
+        super(context, SPAN_COUNT, VERTICAL, true);
         LinearLayout container = (LinearLayout) this.findViewById(R.id.widget_container);
         container.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
         recyclerView.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
-        recyclerView.addItemDecoration(new VerticalSpaceItemDecoration(context){
+        recyclerView.addItemDecoration(new VerticalSpaceItemDecoration(context) {
 
             @Override
             public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
                 super.getItemOffsets(outRect, view, parent, state);
-
+                int resultSize = (int) getResources().getDimension(R.dimen.imoji_search_result_width);
+                int padding = (parent.getWidth() - resultSize * SPAN_COUNT) / (SPAN_COUNT*2);
+                outRect.left = padding;
+                outRect.right = padding;
             }
         });
     }
